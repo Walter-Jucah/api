7dd6f74aad2ec394381e4a802fe80fca367ea1fd
Walter-Jucah <rune.walsh@gmail.com>
2022-08-09 14:01:14 -0300

    adicionado a classe pessoa

--- src/main/java/com/spring/api/controle/Controle.java
+++ src/main/java/com/spring/api/controle/Controle.java
@@ -1,12 +1,30 @@
 package com.spring.api.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.api.Modelo.Pessoa;
 
 @RestController
 public class Controle {
 
+    @GetMapping("")
     public String mensagem() {
         return "Hello World";
     }
 
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Seja bem vindo";
+    }
+
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome) {
+        return "Seja bem vindo" + nome;
+    }
+
+    public Pessoa pessoa() {
+
+    }
 }
