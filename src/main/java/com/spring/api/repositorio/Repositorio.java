8fb4cda9b54e1c33e3ede3ec4b3e745bbd5bd1b4
Walter-Jucah <rune.walsh@gmail.com>
2022-08-18 13:57:35 -0300

    atualziacao controle e respositorio

--- src/main/java/com/spring/api/repositorio/Repositorio.java
+++ src/main/java/com/spring/api/repositorio/Repositorio.java
@@ -1,5 +1,7 @@
 package com.spring.api.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.spring.api.Modelo.Pessoa;
 
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer> {
+
+    List<Pessoa> findAll();
 
 }
